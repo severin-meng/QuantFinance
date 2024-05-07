c90de6f4f8531cff276422fea7be8fff5726c234
severin-meng <severin.meng@gmail.com>
2024-05-07 11:59:33 +0200

    refactoring

--- Equity/volatility/VIX_SPX_LSV/regression.py
+++ Equity/volatility/VIX_SPX_LSV/regression.py
@@ -9,6 +9,7 @@ from load_spx_data import get_prices
 def linear_fit_global():
     data = get_prices()
     data.index = pd.to_datetime(data.index)
+    """
     fig, ax1 = plt.subplots(figsize=(12, 7))
     ax2 = ax1.twinx()
 
@@ -23,9 +24,15 @@ def linear_fit_global():
     ax1.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
     ax2.xaxis.set_major_locator(mdates.YearLocator(base=5))
     ax2.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+    """
+    fig, ax = plt.subplots(figsize=(12, 7))
+    ax.scatter(data['^SPX'], data['^VIX'], marker='.', c=list(range(len(data.index))), s=2)
+
+    ax.set_title('VIX vs SPX')
+    ax.set_ylabel('VIX')
+    ax.set_xlabel('SPX')
 
     plt.show()
-    print("DONE")
 
     pass
 
